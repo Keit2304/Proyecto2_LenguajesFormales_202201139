9f5e7b2da9130496a259f417b87cdaa0aeb0623b
Keit2304 <142373241+Keit2304@users.noreply.github.com>
2024-04-16 22:59:35 -0600

    Nuevo - Guardar como. MainCodeP2.py

--- MainCodeP2.py
+++ MainCodeP2.py
@@ -2,106 +2,96 @@ import tkinter as tk
 from tkinter import messagebox, simpledialog, filedialog
 import os
 
-class IDEWindow:
-    def __init__(self, master):
-        self.master = master
-        master.title("Proyecto #2 - LFP - Keitlyn Tunchez - 202201139")
-
-        window_width = 800  # Ancho 
-        window_height = 600  # Alto 
-        screen_width = master.winfo_screenwidth()
-        screen_height = master.winfo_screenheight()
-        x_coordinate = (screen_width / 2) - (window_width / 2)
-        y_coordinate = (screen_height / 2) - (window_height / 2)
-        master.geometry("%dx%d+%d+%d" % (window_width, window_height, x_coordinate, y_coordinate))
-
-        # Barra de menú
-        self.menubar = tk.Menu(master)
-        master.config(menu=self.menubar)
-
-        # Crear los menús
-        self.file_menu = tk.Menu(self.menubar)
-        self.edit_menu = tk.Menu(self.menubar)
-        self.view_menu = tk.Menu(self.menubar)
-        self.help_menu = tk.Menu(self.menubar)
-
-        self.menubar.add_cascade(label="Archivo", menu=self.file_menu)
-        self.menubar.add_cascade(label="Análisis", menu=self.edit_menu)
-        self.menubar.add_cascade(label="Tokens", menu=self.view_menu)
-        self.menubar.add_cascade(label="Errores", menu=self.help_menu)
-
-        # Opciones del menú Archivo
-        self.file_menu.add_command(label="Nuevo", command=self.nuevo_archivo)
-        self.file_menu.add_command(label="Abrir", command=self.abrir_archivo)
-        self.file_menu.add_command(label="Guardar", command=self.guardar_archivo)
-        self.file_menu.add_command(label="Guardar Como", command=self.guardar_como_archivo)
-        self.file_menu.add_separator()
-        self.file_menu.add_command(label="Salir", command=self.salir)
-
-        # Opción del menú Análisis
-        self.edit_menu.add_command(label="Generar sentencias MongoDB", command=self.generar_mongodb)
-
-        # Opción del menú Tokens
-        self.view_menu.add_command(label="Ver Tokens", command=self.ver_tokens)
-
-        # Crear el área de edición de código
-        self.code_label = tk.Label(master, text="Archivo de entrada:")
-        self.code_label.pack(anchor="w", padx=10, pady=5)
-
-        self.code_area = tk.Text(master, font=("Consolas", 12), height=20, width=100)
-        self.code_area.pack(fill=tk.BOTH, expand=True, padx=10, pady=5)
-
-    def nuevo_archivo(self):
-        # Verificar si hay contenido en el área de edición
-        contenido_actual = self.code_area.get("1.0", "end-1c")
-        if contenido_actual.strip():
-            # Hay contenido, preguntar si desea guardar cambios
-            respuesta = messagebox.askyesnocancel("Nuevo archivo", "¿Desea guardar los cambios en el archivo actual antes de crear uno nuevo?")
-            if respuesta is None:
-                # El usuario canceló la operación
-                return
-            elif respuesta:
-                # El usuario quiere guardar los cambios
-                self.guardar_como_archivo()
+def nuevo_archivo(code_area, master):
     
-        # Limpiar el área de edición
-        self.code_area.delete("1.0", "end")
-
-        # Preguntar el nombre y ruta del nuevo archivo
-        nombre_archivo = simpledialog.askstring("Nuevo archivo", "Ingrese el nombre del nuevo archivo:")
-        if nombre_archivo:
-            ruta_archivo = filedialog.asksaveasfilename(initialfile=nombre_archivo, defaultextension=".txt", filetypes=[("Archivos de texto", "*.txt")])
-            if ruta_archivo:
-                # Actualizar el título de la ventana con el nombre del archivo
-                self.master.title(f"Proyecto #2 - LFP - Keitlyn Tunchez - 202201139 - {os.path.basename(ruta_archivo)}")  
-
-    def abrir_archivo(self):
+    contenido_actual = code_area.get("1.0", "end-1c")
+    if contenido_actual.strip():
         
-        print("Abrir archivo")
+        respuesta = messagebox.askyesnocancel("Nuevo archivo", "¿Desea guardar los cambios en el archivo actual antes de crear uno nuevo?")
+        if respuesta is None:
+            
+            return
+        elif respuesta:
+            
+            guardar_como_archivo(code_area)
+    
+    
+    code_area.delete("1.0", "end")
 
-    def guardar_archivo(self):
-        
-        print("Guardar archivo")
+def abrir_archivo():
+    print("Abrir archivo")
 
-    def guardar_como_archivo(self):
-        
-        print("Guardar como archivo")
+def guardar_archivo():
+    print("Guardar archivo")
 
-    def salir(self):
-        confirmar_salir = messagebox.askyesno("Salir", "¿Está seguro que desea salir del programa?")
-        if confirmar_salir:
-            print("Saliendo del programa")
-            self.master.destroy()
+def guardar_como_archivo(code_area):
+    ruta_archivo = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Archivos de texto", "*.txt")])
+    if ruta_archivo:
+        contenido = code_area.get("1.0", "end-1c")
+        with open(ruta_archivo, "w") as file:
+            file.write(contenido)
 
-    def generar_mongodb(self):
-        
-        print("Generar sentencias MongoDB")
+def salir(master):
+    confirmar_salir = messagebox.askyesno("Salir", "¿Está seguro que desea salir del programa?")
+    if confirmar_salir:
+        print("Saliendo del programa")
+        master.destroy()
 
-    def ver_tokens(self):
-        
-        print("Ver Tokens")
+def generar_mongodb():
+    print("Generar sentencias MongoDB")
 
+def ver_tokens():
+    print("Ver Tokens")
 
-root = tk.Tk()
-ide_window = IDEWindow(root)
-root.mainloop()
+def ventana():
+    root = tk.Tk()
+    root.title("Proyecto #2 - LFP - Keitlyn Tunchez - 202201139")
+
+    window_width = 800  # Ancho 
+    window_height = 600  # Alto 
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x_coordinate = (screen_width / 2) - (window_width / 2)
+    y_coordinate = (screen_height / 2) - (window_height / 2)
+    root.geometry("%dx%d+%d+%d" % (window_width, window_height, x_coordinate, y_coordinate))
+
+    # Barra de menú
+    menubar = tk.Menu(root)
+    root.config(menu=menubar)
+
+    # Crear los menús
+    file_menu = tk.Menu(menubar)
+    edit_menu = tk.Menu(menubar)
+    view_menu = tk.Menu(menubar)
+    help_menu = tk.Menu(menubar)
+
+    menubar.add_cascade(label="Archivo", menu=file_menu)
+    menubar.add_cascade(label="Análisis", menu=edit_menu)
+    menubar.add_cascade(label="Tokens", menu=view_menu)
+    menubar.add_cascade(label="Errores", menu=help_menu)
+
+    # Opciones del menú Archivo
+    file_menu.add_command(label="Nuevo", command=lambda: nuevo_archivo(code_area, root))
+    file_menu.add_command(label="Abrir", command=abrir_archivo)
+    file_menu.add_command(label="Guardar", command=guardar_archivo)
+    file_menu.add_command(label="Guardar Como", command=lambda: guardar_como_archivo(code_area))
+    file_menu.add_separator()
+    file_menu.add_command(label="Salir", command=lambda: salir(root))
+
+    # Opción del menú Análisis
+    edit_menu.add_command(label="Generar sentencias MongoDB", command=generar_mongodb)
+
+    # Opción del menú Tokens
+    view_menu.add_command(label="Ver Tokens", command=ver_tokens)
+
+    # Crear el área de edición de código
+    code_label = tk.Label(root, text="Archivo de entrada:")
+    code_label.pack(anchor="w", padx=10, pady=5)
+
+    code_area = tk.Text(root, font=("Consolas", 12), height=20, width=100)
+    code_area.pack(fill=tk.BOTH, expand=True, padx=10, pady=5)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    ventana()
